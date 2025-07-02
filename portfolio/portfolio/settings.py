067c1b8250f744d39afbd2fb6393406afd858096
Adelodun Peter <adelodunpeter69@gmail.com>
2025-07-02 13:33:23 +0100

    fixed some bugs

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio.app/static'),  
+    os.path.join(BASE_DIR, 'portfolio_app/static'),  
     
 ]
 
