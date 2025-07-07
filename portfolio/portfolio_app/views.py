fbc41cf1d6b7a74ce8fd2aeea01d19411fa023ed
Adelodunpeter25 <adelodunpeter69@gmail.com>
2025-07-07 13:59:35 +0100

    edited the frontend

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -16,14 +16,4 @@ def contact(request):
     return render(request, 'contact.html', {'title': 'My Portfolio - Contact'})
 
 def form_submission(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-
-        if name and email and message:
-            return HttpResponse("Thank you! Your message has been sent.")
-        else:
-            return HttpResponse("Please fill out all fields.", status=400)
-
     return render(request, 'form_submission.html', {'title': 'My Portfolio - Form Submission'})
